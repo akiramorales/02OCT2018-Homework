6c5d4f3ed9ac154893a0a2540ba141921ab538ea
Akira Morales <akira.k.morales@gmail.com>
2018-10-02 13:37:28 -0400

    Add files via upload

---
+++ src/RectangleInput.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Akira Morales
+ * 02 OCT 2018 Homework
+ */
+import java.util.Scanner;
+
+public class RectangleInput {
+
+	public static void main(String[] args)
+	{
+		double width = new RectangleInp;
+		Scanner length = new Scanner(System.in);
+		
+		System.out.print("Input the width of the rectangle: " width.nextDouble);
+		// use /t as well
+		// length is double
+		
+	}
+
+}
