12b1bb7088d99ab946c3212c0cbb54b924624a0f
AKIRA <AKIRA@AKIRA-PC.fios-router.home>
2018-10-02 22:47:08 -0400

    Finished assignment

--- src/RectangleInput.java
+++ src/RectangleInput.java
@@ -9,12 +9,24 @@ public class RectangleInput {
 
 	public static void main(String[] args)
 	{
-		double width = new RectangleInp;
-		Scanner length = new Scanner(System.in);
+		Scanner wth = new Scanner(System.in);
+		Scanner lgth = new Scanner(System.in);
 		
-		System.out.print("Input the width of the rectangle: " width.nextDouble);
-		// use /t as well
-		// length is double
+		System.out.print("Input the width of the rectangle: ");
+		double width = wth.nextDouble();
+		wth.nextLine();
+		
+		System.out.print("Input the length of the rectangle: ");
+		double length = lgth.nextDouble();
+		lgth.nextLine();
+		
+		double area = length * width;
+		double perimeter = 2 * length + 2 * width;
+		double diagonal = Math.sqrt(length * length + width * width);
+		
+		System.out.printf("Area:  %18.2f\n", area);
+		System.out.printf("Perimeter:  %13.2f\n", perimeter);
+		System.out.printf("Length of diagonal:  %3.2f", diagonal);
 		
 	}
 
